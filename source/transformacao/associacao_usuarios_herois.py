5215d3a255a19854e0e9617da02c55530b490e3c
matheus <cardosomcds1@gmail.com>
2026-03-11 18:46:17 -0300

    Salvando o arquivo em camada silver com pipeline de execução.

--- source/transformacao/associacao_usuarios_herois.py
+++ source/transformacao/associacao_usuarios_herois.py
@@ -1,24 +1,42 @@
 from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.functions import col
 from source.transformacao.transformacao_usuarios import TranformacaoUsuarios
 from source.transformacao.transformacao_herois import TransformacaoHerois
 import logging
+from dotenv import load_dotenv
+from os import getenv
 
 logger = logging.getLogger(__name__)
 
 
 class AssociacaoUsuariosHerois:
+    load_dotenv()
+    SILVER_PATH_FILE = getenv('SILVER_PATH_FILE')
 
-    def __init__(self):
+    def __init__(self, spark_session: SparkSession):
         """
         Inicializa a classe AssociacaoUsuariosHerois e Cria uma sessão Spark.
         """
-        self.spark = SparkSession.\
-            builder.\
-            appName('usuarios_e_herois').\
-            getOrCreate()
-        self.spark.sparkContext.setLogLevel("ERROR")
+        self.spark = spark_session
         self.transformacao_usuarios = TranformacaoUsuarios(self.spark)
         self.transformacao_herois = TransformacaoHerois(self.spark)
+
+    def executa_pipeline(self):
+        df_herois_e_usuarios = self.associar_herois_e_usuarios()
+        self.salva_dataframe_parquet(df_herois_e_usuarios)
+
+    def salva_dataframe_parquet(self, df_herois_e_usuarios: DataFrame):
+        if not self.SILVER_PATH_FILE:
+            raise ValueError('Caminho silver path não encontrado')
+
+        df_herois_e_usuarios.write.parquet(
+            path=self.SILVER_PATH_FILE,
+            mode='overwrite',
+            compression='snappy'
+        )
+
+        msg = f'DataFrame silver salvo em: "{self.SILVER_PATH_FILE}."'
+        logger.info(msg)
 
     def associar_herois_e_usuarios(self) -> DataFrame:
         """
@@ -37,7 +55,7 @@ class AssociacaoUsuariosHerois:
         )
 
         df_herois_e_usuarios = df_herois_e_usuarios.select(
-            'nome',
+            col('nome').alias('nome_completo'),
             'email',
             'telefone',
             'telefone_numerico',
@@ -47,21 +65,24 @@ class AssociacaoUsuariosHerois:
             'nota',
             'data_execucao',
             'heroi_id',
-            'name',
-            'Gender',
-            'Eye_color',
-            'Race',
-            'Hair_color',
-            'Height',
-            'Alignment',
-            'Weight'
+            col('name').alias('nome_heroi'),
+            col('Gender').alias('genero_heroi'),
+            col('Eye_color').alias('cor_olho_heroi'),
+            col('Race').alias('especie_heroi'),
+            col('Hair_color').alias('cor_cabelo_heroi'),
+            col('Height').alias('altura_cm_heroi'),
+            col('Alignment').alias('Alinhamento_heroi'),
+            col('Weight').alias('peso_heroi')
         )
-
+        df_herois_e_usuarios.show(10)
         logger.info('Associação Concluída com sucesso.')
         return df_herois_e_usuarios
 
 
 if __name__ == "__main__":
-    associacao = AssociacaoUsuariosHerois()
-    df_herois_e_usuarios = associacao.associar_herois_e_usuarios()
-    df_herois_e_usuarios.show()
+    spark = SparkSession.\
+            builder.\
+            appName('usuarios_e_herois').\
+            getOrCreate()
+    associacao = AssociacaoUsuariosHerois(spark_session=spark)
+    associacao.executa_pipeline()
