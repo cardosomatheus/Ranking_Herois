f32f93265590879d536ced69c1f05a4aeb2b4387
matheus <cardosomcds1@gmail.com>
2026-03-14 18:52:39 -0300

    Reestruturando a camada gold.

--- source/load/salva_parquet_load.py
+++ source/load/salva_parquet_load.py
@@ -9,38 +9,64 @@ logger = logging.getLogger(__name__)
 
 class SalvaParquetLoad:
     """
-    Classe responsável por salvar um DataFrame em formato Parquet.
     A classe utiliza a biblioteca PySpark para realizar escrita do Parquet.
-    """
+    Classe responsável por:
+        Ler o arquivo parquet já transformado na camada SILVER.
+        Adicionar as novas informação no arquivo parquet na camada GOLD.
+     """
     load_dotenv()
-    output_path = os.getenv("PATH_FILE_OUTPUT_PARQUET")
+    SILVER_PATH_FILE = os.getenv('SILVER_PATH_FILE')
+    GOLD_PATH_FILE = os.getenv('GOLD_PATH_FILE')
 
     def __init__(self, spark: SparkSession):
         """Inicializa a classe com uma instância do SparkSession."""
         self.spark = spark
+        self.name_file = 'file_processed_load.parquet'
 
-    def load_dataframe_to_parquet(self, dataframe: DataFrame) -> None:
-        """Salva o DataFrame em formato Parquet no caminho especificado."""
-        if dataframe.take(1) == []:
-            msg = 'O DataFrame está vazio, não é possível salvar em parquet.'
+    def executa_pipeline(self):
+        """ Executa a parte do load em GOLD PATH"""
+        dataframe_gold = self.read_datafrane_parquet()
+        self.salva_dataframe_parquet_gold(dataframe_gold)
+
+    def read_datafrane_parquet(self) -> DataFrame:
+        """Leitura do arquivo parquet no SILVER PATH."""
+        logger.info('Lendo DataFrame da SILVER PATH.')
+
+        if not self.SILVER_PATH_FILE:
+            msg = 'Silver path não encontrado no load final.'
             logger.error(msg)
             raise ValueError(msg)
 
+        dataframe = self.spark.read.parquet(self.SILVER_PATH_FILE)
+        dataframe.show(5)
+        return dataframe
+
+    def salva_dataframe_parquet_gold(self, dataframe: DataFrame) -> None:
+        """Salva o DataFrame em formato Parquet no GOLD PATH."""
+        logger.info('Salvando DataFrame na GOLD PATH.')
+        if not self.GOLD_PATH_FILE:
+            msg = 'Gold path não encontrado'
+            logger.error(msg)
+            raise ValueError(msg)
+
+        if dataframe.take(1) == []:
+            msg = f'O DF está vazio em SILVER PATH: {self.SILVER_PATH_FILE}'
+            logger.info(msg)
+            return
+
         dataframe.write.parquet(
-            path=self.output_path,
-            mode='overwrite',
+            path=f'{self.GOLD_PATH_FILE}/{self.name_file}',
+            mode='append',
             compression='snappy'
         )
-        logger.info(f"DataFrame salvo com sucesso em {self.output_path}")
+        logger.info(f"DataFrame salvo com sucesso em {self.GOLD_PATH_FILE}")
 
 
 if __name__ == "__main__":
-    from source.transformacao.associacao_usuarios_herois import (
-        AssociacaoUsuariosHerois
-    )
+    spark = SparkSession.\
+            builder.\
+            appName('usuarios_e_herois').\
+            getOrCreate()
 
-    associacao = AssociacaoUsuariosHerois()
-    df_herois_e_usuarios = associacao.associar_herois_e_usuarios()
-
-    classe_load = SalvaParquetLoad(associacao.spark)
-    classe_load.load_dataframe_to_parquet(df_herois_e_usuarios)
+    classe_load = SalvaParquetLoad(spark=spark)
+    classe_load.executa_pipeline()
